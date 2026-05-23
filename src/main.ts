7dc462a81cfa74c3d0d100af92d0288083fb2a45
Bastian Onate <bastian@zipco.cl>
2026-05-23 18:52:27 -0400

    Agregar URL de Vercel al CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://zipco-app.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
